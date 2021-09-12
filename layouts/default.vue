48dc331a24e9b1d8ed54121ccd488667a81bdc57
Douglas Glover <douglas.glover@outlook.com>
2021-09-12 13:41:44 -0400

    made some style adjustments based on feedback

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,22 @@
 <template>
   <div class="py-5">
     <SiteHeader />
-    <Nuxt />
+    <transition name="slide-fade">
+      <Nuxt />
+    </transition>
   </div>
 </template>
 
-<style></style>
+<style>
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(20px);
+  opacity: 0;
+}
+</style>
