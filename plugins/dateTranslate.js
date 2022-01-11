f94be3f58b82f564af4c111e7156e501efbd6bcd
DouglasKGlover <douglas.glover@outlook.com>
2022-01-10 23:58:17 -0500

    updated month on dateTranslate with +1

--- plugins/dateTranslate.js
+++ plugins/dateTranslate.js
@@ -1,9 +1,8 @@
 export default (context, inject) => {
   const dateTranslate = (dateSubmitted) => {
     let newDate = new Date(dateSubmitted);
-    newDate = `${newDate.getDate()}/${newDate.getMonth()}/${
-      newDate.getFullYear() - 2000
-    }`;
+    newDate = `${newDate.getDate()}/${newDate.getMonth() + 1}/${newDate.getFullYear() - 2000
+      }`;
     return newDate;
   };
   inject("dateTranslate", dateTranslate);
