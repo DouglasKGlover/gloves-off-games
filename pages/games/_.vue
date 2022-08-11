36c7b59af81afe61666946988d6d4ac0de3feba1
DouglasKGlover <douglas.glover@outlook.com>
2022-08-11 09:47:56 -0400

    added a price check feature

--- pages/games/_.vue
+++ pages/games/_.vue
@@ -30,18 +30,22 @@
               <strong>Requirements for Completion:</strong>
               {{ game.requirementsForCompletion }}
             </li>
+
+            <li>
+              <a :href="ebayLink" target="_blank">Price Check</a>
+            </li>
           </ul>
         </b-col>
       </b-row>
 
-      <b-row v-if="gameData">
+      <!-- <b-row v-if="gameData">
         <b-col>
           <div v-for="(game, index) in gameData" :key="`game-data-${index}`">
             <img v-if="game.cover" :src="game.cover" class="mt-3" />
             <p v-if="game.summary" v-html="game.summary" class="mt-3"></p>
           </div>
         </b-col>
-      </b-row>
+      </b-row> -->
 
       <div v-if="game.photosCollection.items.length" class="mt-5">
         <b-row>
@@ -127,6 +131,14 @@ export default {
     //   this.gameData = JSON.parse(xhr.response);
     // };
     // xhr.send();
+  },
+  computed: {
+    ebayLink() {
+      const title = this.game.title.replaceAll(" ", "+");
+      const systemTitle = this.game.system.title.replaceAll(" ", "+");
+      let searchTerm = `https://www.ebay.ca/sch/i.html?_nkw=${title}+${systemTitle}&_in_kw=1&_ex_kw=&_sacat=0&LH_Sold=1&_udlo=&_udhi=&_samilow=&_samihi=&_sadis=15&_stpos=M4V+2E9&_sargn=-1%26saslc%3D1&_salic=2&_sop=12&_dmd=1&_ipg=60&LH_Complete=1&_fosrp=1`;
+      return searchTerm;
+    },
   },
 };
 </script>
