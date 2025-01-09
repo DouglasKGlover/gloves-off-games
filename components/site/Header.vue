376704e5e64b083b8e50caccc68273daa7890d84
DouglasKGlover <douglas.glover@outlook.com>
2025-01-08 22:41:42 -0400

    hamburger moved

--- components/site/Header.vue
+++ components/site/Header.vue
@@ -100,7 +100,7 @@ header {
 
     .toggle {
       position: fixed;
-      top: calc(100vh - 50px - 1em - var(--safe-area-inset-bottom));
+      top: calc(100vh - 50px - 1em - 100px);
       left: calc(100vw - 50px - 1em);
       width: 50px;
       height: 50px;
