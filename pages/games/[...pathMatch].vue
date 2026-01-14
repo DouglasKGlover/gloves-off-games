eedacc1e27f14479e1daeb0ff34861792f70ecfa
DouglasKGlover <douglas.glover@outlook.com>
2026-01-14 00:28:11 -0400

    game page

--- pages/games/[...pathMatch].vue
+++ pages/games/[...pathMatch].vue
@@ -8,92 +8,85 @@
       </template>
     </SiteHero>
     <div class="container" v-if="game.title">
-      <div>
-        <div>
-          <!-- Overview -->
-          <div class="section">
-            <h3>Overview</h3>
+      <!-- Overview -->
+      <section class="game-overview">
+        <div v-if="game.requirementsForCompletion" class="overview-row">
+          <div class="overview-label">Requirements for Completion:</div>
+          <div
+            class="overview-value"
+            v-html="$translateLongText(game.requirementsForCompletion)"
+          />
+        </div>
 
-            <ul>
-              <li>
-                <GameRegionIndicator :region="game.region" />
-              </li>
-              <li v-if="game.requirementsForCompletion">
-                <strong>Requirements for Completion:</strong>
-                <div
-                  v-html="$translateLongText(game.requirementsForCompletion)"
-                />
-              </li>
+        <div class="overview-row">
+          <div class="overview-label">Status:</div>
+          <div class="overview-value">{{ game.playedStatus }}</div>
+        </div>
 
-              <li>
-                <strong>Played Status: </strong>
-                <GamePlayedStatusIndicator :status="game.playedStatus" />{{
-                  game.playedStatus
-                }}
-              </li>
+        <div v-if="game.highScore" class="overview-row">
+          <div class="overview-label">High Score:</div>
+          <div class="overview-value">{{ game.highScore }}</div>
+        </div>
 
-              <li v-if="game.highScore">
-                <strong>High Score: </strong>
-                {{ game.highScore }}
-              </li>
-
-              <li>
-                <strong>Added: </strong>
-                {{ $dateTranslate(game.sys.firstPublishedAt).long }}
-              </li>
-
-              <li>
-                <strong>Updated: </strong>
-                {{ $dateTranslate(game.sys.publishedAt).long }}
-              </li>
-
-              <li>
-                <a :href="ebayLink" target="_blank">&#128184; Price Check</a>
-              </li>
-            </ul>
-
-            <p></p>
-          </div>
-
-          <!-- Photos -->
-          <div v-if="photosList.length" class="section">
-            <h3>Photos</h3>
-
-            <div>
-              <div
-                v-for="(photo, index) in photosList"
-                :key="`game-photo-${index}`"
-              >
-                <button
-                  @click="openModal(`photo-modal-${index}`)"
-                  class="image-button"
-                >
-                  <img :src="photo.thumbnail" width="300" height="200" />
-                </button>
-
-                <dialog :id="`photo-modal-${index}`">
-                  <img :src="photo.url" />
-                </dialog>
-              </div>
-            </div>
-          </div>
-
-          <!-- Game Logs -->
-          <div v-if="glogs.length" class="section">
-            <h3>Game Log<span v-if="glogs.length > 1">s</span></h3>
-            <div
-              class="game-log-link"
-              v-for="(glog, index) in glogs"
-              :key="`game-log-${index}`"
-            >
-              {{ $dateTranslate(glog.sys.firstPublishedAt).short }} -
-              <NuxtLink :to="`/blog/${glog.slug}`">
-                {{ glog.title }}
-              </NuxtLink>
-            </div>
+        <div class="overview-row">
+          <div class="overview-label">Added:</div>
+          <div class="overview-value">
+            {{ $dateTranslate(game.sys.firstPublishedAt).long }}
           </div>
         </div>
-      </div>
+
+        <div class="overview-row">
+          <div class="overview-label">Updated:</div>
+          <div class="overview-value">
+            {{ $dateTranslate(game.sys.publishedAt).long }}
+          </div>
+        </div>
+
+        <div class="overview-row">
+          <!-- <div class="overview-label">Price Check:</div> -->
+          <div class="overview-value">
+            <a :href="ebayLink" target="_blank">&#128184; Price Check</a>
+          </div>
+        </div>
+      </section>
+
+      <!-- Photos -->
+      <section v-if="photosList.length" class="section">
+        <hr />
+        <h3>Photos</h3>
+        <div class="grid grid-cols-2 grid-cols-md-4 grid-cols-lg-6">
+          <div
+            v-for="(photo, index) in photosList"
+            :key="`game-photo-${index}`"
+            class="game-photo-item"
+          >
+            <img
+              :src="photo.thumbnail"
+              :alt="`Photo ${index + 1}`"
+              class="game-photo-img"
+              width="300"
+              height="200"
+            />
+          </div>
+        </div>
+      </section>
+
+      <!-- Blog -->
+      <section v-if="glogs.length" class="section">
+        <hr />
+
+        <h3>Blog</h3>
+        <div
+          class="game-log-link"
+          v-for="(glog, index) in glogs"
+          :key="`game-log-${index}`"
+        >
+          {{ $dateTranslate(glog.sys.firstPublishedAt).short }} -
+          <NuxtLink :to="`/blog/${glog.slug}`">
+            {{ glog.title }}
+          </NuxtLink>
+        </div>
+      </section>
     </div>
   </div>
 </template>
@@ -103,13 +96,6 @@ import gameBySlugAndSystemQuery from "~/graphql/gameBySlugAndSystem.gql";
 
 const route = useRoute();
 const { $graphql } = useNuxtApp();
-// const parsePathMatch = (pathArray) => {
-//   // pathArray is [system, slug] from [system]/[slug]
-//   return {
-//     slug: pathArray[1],
-//     system: pathArray[0],
-//   };
-// };
 
 // Normalize catch-all param to segments [system, slug]
 const pathParam = route.params.pathMatch;
@@ -152,4 +138,45 @@ useHead({
 
 <style scoped lang="scss">
 @use "~/assets/css/breakpoints" as *;
+
+.game-overview {
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+  background: #181a20;
+  border-radius: 0.8rem;
+  padding: 2rem;
+  margin-bottom: 2rem;
+}
+.overview-row {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  align-items: flex-start;
+}
+.overview-label {
+  min-width: 140px;
+  font-weight: 600;
+  color: #b3b6c2;
+}
+.overview-value {
+  flex: 1 1 0;
+  color: #f7f7fa;
+}
+.game-photo-item {
+  width: 100%;
+  aspect-ratio: 3/2;
+  overflow: hidden;
+  border-radius: 0.5rem;
+  background: #181a20;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.game-photo-img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  display: block;
+}
 </style>
